7ee2acf717564c305c3bdb7aee9d6560594ba5f0
Mehdi-133 <mehdikarbitou@gmail.com>
2025-11-18 12:20:55 +0100

    add  media queries for  mobile

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-const add = document.getElementById("add-btn");
+const newWorker = document.getElementById("add-btn");
 const form = document.querySelector(".form");
+const addList = document.getElementById("addList");
+const plus = document.querySelectorAll(".plus");
+const remove = document.getElementById("remove");
+const canceled = document.getElementById("canceled");
+
+storedData = []
 
 
-add.addEventListener("click", () => {
-  form.style.display = "flex"; 
+
+newWorker.addEventListener("click", () => {
+  form.style.display = "flex";
 });
 
+canceled.addEventListener("click", () => {
+  form.style.display = "none";
+});
+
+plus.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    addList.style.display = "flex";
+  });
+});
+
+remove.addEventListener("click", () => {
+  addList.style.display = "none";
+});
